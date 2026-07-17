1ae98ad7c0c7ea435d8aef414349cc29efec8721
emadhosseini <emad.hosseini89@gmail.com>
2026-07-17 10:24:18 +0400

    Setup Tailwind CSS and Persian font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import "@fontsource/vazirmatn/400.css";
+import "@fontsource/vazirmatn/500.css";
+import "@fontsource/vazirmatn/700.css";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
